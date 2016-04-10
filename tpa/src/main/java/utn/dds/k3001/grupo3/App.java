21a3ee96e667f4c5340387db33ae717e93e89318
Juani Sierra <juanignaciosierra96@gmail.com>
2016-04-10 18:15:30 -0300

    Creo el proyecto maven y agrego el gitignore

---
+++ tpa/src/main/java/utn/dds/k3001/grupo3/App.java
@@ -0,0 +1,13 @@
+package utn.dds.k3001.grupo3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

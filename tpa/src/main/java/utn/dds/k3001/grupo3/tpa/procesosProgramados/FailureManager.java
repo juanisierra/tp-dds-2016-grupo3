8537c0fa47189416453747d519920fe339577692
Juani Sierra <juanignaciosierra96@gmail.com>
2016-07-04 19:21:57 -0300

    Agrego FailureManager Closes #15 #16

---
+++ tpa/src/main/java/utn/dds/k3001/grupo3/tpa/procesosProgramados/FailureManager.java
@@ -0,0 +1,24 @@
+package utn.dds.k3001.grupo3.tpa.procesosProgramados;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Callable;
+
+public class FailureManager implements Runnable {
+	Callable<ResultadoProceso> proceso;
+	SchedulerProcesos scheduler;
+	public FailureManager(Callable<ResultadoProceso> proceso,SchedulerProcesos scheduler)
+	{
+		this.proceso = proceso;
+		this.scheduler = scheduler;
+	}
+	@Override
+	public void run() {
+	try {
+		ResultadoProceso resultado = proceso.call();
+		scheduler.registrarResultado(resultado);
+	} catch (Exception e) {
+		scheduler.registrarResultado(new ResultadoProceso(LocalDateTime.now(),0, false, e.getMessage()));
+	}
+	}
+
+}
--- tpa/src/main/java/utn/dds/k3001/grupo3/tpa/procesosProgramados/SchedulerProcesos.java
+++ tpa/src/main/java/utn/dds/k3001/grupo3/tpa/procesosProgramados/SchedulerProcesos.java
@@ -5,33 +5,26 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class SchedulerProcesos
 {
 	private ScheduledExecutorService executor;
-	private List<ScheduledFuture<ResultadoProceso>> historialProcesos;
+	private List<ResultadoProceso> historialProcesos;
 	
 	public SchedulerProcesos(){
 		executor = Executors.newSingleThreadScheduledExecutor();
-		historialProcesos = new ArrayList<ScheduledFuture<ResultadoProceso>>();
+		historialProcesos = new ArrayList<ResultadoProceso>();
 	}
 	public List<ResultadoProceso> getHistorial() {
-		List<ResultadoProceso> lista = new ArrayList<ResultadoProceso>();
-		historialProcesos.stream().filter(future -> future.isDone()).forEach(resultado ->{
-			try{
-				lista.add(resultado.get());
-			} catch (InterruptedException | ExecutionException exception){
-				exception.printStackTrace();
-			}
-		});
-		return lista;
+	return historialProcesos;
 	}
 	public void agregarTarea(Callable<ResultadoProceso> tarea,LocalDateTime horaEjecucion){
-		historialProcesos.add(executor.schedule(tarea, ChronoUnit.SECONDS.between(LocalDateTime.now(), horaEjecucion), TimeUnit.SECONDS));
+	executor.schedule(new FailureManager(tarea,this), ChronoUnit.SECONDS.between(LocalDateTime.now(), horaEjecucion), TimeUnit.SECONDS);
+	}
+	public void registrarResultado(ResultadoProceso resultado) {
+	historialProcesos.add(resultado);
 	}
 }

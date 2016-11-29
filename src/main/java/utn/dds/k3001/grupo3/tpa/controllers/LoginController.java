7c720624974b345112f2175a701e42fe4e23bc9b
santipecin <santi_pecin@hotmail.com>
2016-11-29 15:48:26 -0300

    saco if closes #45

--- src/main/java/utn/dds/k3001/grupo3/tpa/controllers/LoginController.java
+++ src/main/java/utn/dds/k3001/grupo3/tpa/controllers/LoginController.java
@@ -8,36 +8,30 @@ import spark.Response;
 import utn.dds.k3001.grupo3.tpa.usuarios.RepositorioUsuarios;
 
 public class LoginController {
+	
 	public ModelAndView mostrarLogin(Request req, Response res){
 		return new ModelAndView(null, "login/login.hbs");
 	}
+	
 	public ModelAndView iniciarSesion(Request req, Response res){
 		String id_usuario = req.queryParams("usuario");
 		String contrasenia = req.queryParams("contrasenia");
 		req.session().removeAttribute("user");
-		if(!RepositorioUsuarios.instance().validarUsuario(id_usuario, contrasenia))
-		{
+		if(RepositorioUsuarios.instance().validarUsuario(id_usuario, contrasenia)){
+			req.session().attribute("user",RepositorioUsuarios.instance().getUsuario(id_usuario));
+			res.redirect("/pois");
+			return null;
+		}
+		else{
 			Map<String,String> model = new HashMap<>();
 			model.put("error","Error: El usuario o la contraseña no son validos");
 			return new ModelAndView(model, "login/login.hbs");
-		} else {
-			req.session().attribute("user",RepositorioUsuarios.instance().getUsuario(id_usuario));
-		if(RepositorioUsuarios.instance().getUsuario(id_usuario).esAdmin()){
-			
-			res.redirect("/pois");
-			
-			return null;
 		}
-		else {
-			res.redirect("/pois");
-			return null;
-		}
-		} 
 	}
+	
 		public Void logout(Request req, Response res){
 			req.session().removeAttribute("user");
 			res.redirect("/login");
 			return null;
 		}
-	
 }
